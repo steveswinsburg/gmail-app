ebda5e67cab57227fc939df320a6bebaee551d1f
Steve Swinsburg <steve.swinsburg@orionhealth.com>
2020-11-09 12:08:38 +1100

    Initial commit

--- README.md
+++ README.md
@@ -3,5 +3,16 @@ A MacOS app you can use to launch Gmail
 
 Dockable and privacy is guaranteed, this apps stores no personal information.
 
+## Features
+* Use Gmail like any MacOS app
+* Add it to your dock
+* Forget about browser tabs
+
+### Getting started
+- Install [Node](https://nodejs.org)
+- Clone this repository
+- `npm install`
+- `npm start`
+
 ---
 Not produced or endorsed by Google.
---
+++ app.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron') // http://electron.atom.io/docs/api
+
+const url = "https://mail.google.com/mail/u/0/#inbox"
+
+let window = null
+
+// Wait until the app is ready
+app.once('ready', () => {
+  
+  // Create a new window
+  window = new BrowserWindow({
+    // Set the initial width to 800px
+    width: 800,
+    // Set the initial height to 600px
+    height: 600,
+    // Don't show the window until it ready, this prevents any white flickering
+    show: false,
+    webPreferences: {
+      // Disable node integration in remote page
+      nodeIntegration: false
+    }
+  })
+
+  //open window
+  window.loadURL(url)
+
+  // Show window when page is ready
+  window.once('ready-to-show', () => {
+    window.maximize()
+    window.show()
+  })
+})
